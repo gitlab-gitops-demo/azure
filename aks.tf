a180a4cbcb36a9ef96ddd063d2b03ff0b30e6510
Brad Downey <bdowney@gitlab.com>
2019-10-01 10:32:25 -0700

    Add AKS Cluster

--- aks.tf
+++ aks.tf
@@ -7,3 +7,39 @@ resource "azurerm_resource_group" "gitops-demo" {
   location = "West US 2"
 }
 
+# Set Environment Variable TF_VAR_client_id=00000000-0000-0000-0000-000000000000
+variable "client_id" {}
+# Set Environment Variable TF_VAR_client_secret=00000000000000000000000000000000
+variable "client_secret" {}
+
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = "gitops-demo"
+  location            = "${azurerm_resource_group.gitops-demo.location}"
+  resource_group_name = "${azurerm_resource_group.gitops-demo.name}"
+  dns_prefix          = "gitops-demo"
+
+  agent_pool_profile {
+    name            = "default"
+    count           = 1
+    vm_size         = "Standard_D1_v2"
+    os_type         = "Linux"
+    os_disk_size_gb = 30
+  }
+
+  agent_pool_profile {
+    name            = "pool2"
+    count           = 1
+    vm_size         = "Standard_D2_v2"
+    os_type         = "Linux"
+    os_disk_size_gb = 30
+  }
+
+  service_principal {
+    client_id     = "${var.client_id}"
+    client_secret = "${var.client_secret}"
+  }
+
+  tags = {
+    Terraform = "True"
+  }
+}
