c8163dbcf4337a58e56134b1afdf55b10ea0b04d
Brad Downey <bdowney@gitlab.com>
2020-04-22 20:39:26 +0000

    Update group_cluster.tf

--- group_cluster.tf
+++ group_cluster.tf
@@ -9,7 +9,7 @@ resource "gitlab_group_cluster" "aks_cluster" {
   group              = data.gitlab_group.gitops-demo-apps.id
   name               = azurerm_kubernetes_cluster.aks.name
   domain             = "aks.gitops-demo.com"
-  environment_scope  = "*"
+  environment_scope  = "aks/*"
   kubernetes_api_url = azurerm_kubernetes_cluster.aks.kube_config.0.host
   kubernetes_token   = data.kubernetes_secret.gitlab-admin-token.data.token
   kubernetes_ca_cert = trimspace(base64decode(azurerm_kubernetes_cluster.aks.kube_config.0.cluster_ca_certificate))
