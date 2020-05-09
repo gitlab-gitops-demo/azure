396241ce6e832da21e47f08d00dde79faaa5c2de
Brad Downey <bdowney@gitlab.com>
2020-05-09 14:36:01 -0700

    Update azurerm

--- aks.tf
+++ aks.tf
@@ -1,5 +1,6 @@
 provider "azurerm" {
-  version = "=1.34.0"
+  version = "=2.9.0"
+  features {}
 }
 
 resource "azurerm_resource_group" "gitops-demo" {
@@ -7,28 +8,21 @@ resource "azurerm_resource_group" "gitops-demo" {
   location = "West US 2"
 }
 
-# Set Environment Variable TF_VAR_client_id=00000000-0000-0000-0000-000000000000
-variable "client_id" {}
-# Set Environment Variable TF_VAR_client_secret=00000000000000000000000000000000
-variable "client_secret" {}
-
 resource "azurerm_kubernetes_cluster" "aks" {
   name                = "gitops-demo-aks"
   location            = azurerm_resource_group.gitops-demo.location
   resource_group_name = azurerm_resource_group.gitops-demo.name
   dns_prefix          = "gitlab"
 
-  agent_pool_profile {
+  default_node_pool {
     name            = "default"
-    count           = 3
+    node_count      = 3
     vm_size         = "Standard_F2s_v2"
-    os_type         = "Linux"
     os_disk_size_gb = 30
   }
 
-  service_principal {
-    client_id     = var.client_id
-    client_secret = var.client_secret
+  identity {
+    type = "SystemAssigned"
   }
 
   tags = {
