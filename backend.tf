5cb62eb269619c2909777a4120540827acc70e0d
Brad Downey <bdowney@gitlab.com>
2020-05-16 07:18:48 -0700

    Use GitLab TF Backend

--- backend.tf
+++ backend.tf
@@ -1,10 +1,11 @@
 terraform {
-  backend "remote" {
-    hostname     = "app.terraform.io"
-    organization = "gitops-demo"
-
-    workspaces {
-      name = "azure"
-    }
+  backend "http" {
+    address        = "https://gitlab.com/api/v4/projects/14379215/terraform/state/azure"
+    lock_address   = "https://gitlab.com/api/v4/projects/14379215/terraform/state/azure/lock"
+    unlock_address = "https://gitlab.com/api/v4/projects/14379215/terraform/state/azure/lock"
+    username       = "tf"
+    lock_method    = "POST"
+    unlock_method  = "DELETE"
+    retry_wait_min = "5"
   }
 }
