3d7f7115abeeff12d7260f2d8e371fadd290221f
Brad Downey <bdowney@gitlab.com>
2020-09-22 11:45:39 -0700

    Change backend to env vars

--- backend.tf
+++ backend.tf
@@ -1,11 +1,3 @@
 terraform {
   backend "http" {
-    address        = "https://gitlab.com/api/v4/projects/14379215/terraform/state/azure"
-    lock_address   = "https://gitlab.com/api/v4/projects/14379215/terraform/state/azure/lock"
-    unlock_address = "https://gitlab.com/api/v4/projects/14379215/terraform/state/azure/lock"
-    username       = "tf"
-    lock_method    = "POST"
-    unlock_method  = "DELETE"
-    retry_wait_min = "5"
-  }
 }
